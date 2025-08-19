3113adff718bf8b0f2c7f27c73a14ee9297ff5b8
susanzh4ng <Susan.Zhang901@gmail.com>
2025-08-19 14:21:53 -0500

    updated CardSZ component

--- src/components/CardSZ/CardSZ.js
+++ src/components/CardSZ/CardSZ.js
@@ -1,6 +1,6 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { PiDiamondsFourFill } from "react-icons/pi";
+import { PiStorefrontFill, PiPenNibFill, PiBracketsCurlyFill } from "react-icons/pi";
 import { useNavigate } from 'react-router-dom'
 import "./CardSZ.css";
 
@@ -22,9 +22,11 @@ function CardSZ(props){
                 <Col md={6}>
                     <img src={props.coverImg} alt="Hero" className="img-fluid coverImg"/>
                 </Col>
-                <Col md={6} className='text'>
+                <Col md={6} className="d-flex flex-column justify-content-center mt-2 mt-md-0 text">
                     <span className="d-inline-flex align-items-center">
-                        <PiDiamondsFourFill size="35" className="me-1" />
+                        {props.marketingPage && <PiStorefrontFill size="25" className="me-1" />}
+                        {props.uiuxPage && <PiPenNibFill size="25" className="me-1" />}
+                        {props.fullStackPage && <PiBracketsCurlyFill size="25" className="me-1" />}
                         <b>{props.title}</b>
                     </span>
                     <p className="description">{props.description}</p>
